5e41dedc90f7c1177b53312fe52736bb94de831e
TorrentBaz <129433252+TorrentBaz@users.noreply.github.com>
2026-09-24 15:37:05 +0330

    chore: normalize source line endings

--- src/fulfillment/worker.rs
+++ src/fulfillment/worker.rs
@@ -7,40 +7,44 @@ use crate::{fulfillment::FulfillmentResult, state::AppState};
 pub async fn run(state: AppState) {
     let worker_id = format!("worker-{}", uuid::Uuid::new_v4().simple());
     loop {
-        match state.db.claim_next_fulfillment_job('­½òÚ$z{-®éÜj×,vã6)öaßH‹ˆÜ™\‹œX›X×ÚYˆÜ™\‹œÙ\šXÙWÝ]KˆÜ™\‹\™Ù]Ý\Ù\›˜[YKˆ›Ü›X]ÜšX[
-Ü™\‹˜[[Ý[ÜšX[
-KˆÜ™\‹œÝ]\Âˆ
-BˆJBˆ˜ÛÛXÝŽ™XÏÏŠ
-Bˆš›Ú[Š——ˆŠBŸB‚™›ˆ›Ü›X]ÜšX[
-[[Ý[ˆM
-HOˆÝš[™ÈÂˆ]YÚ]ÈH[[Ý[˜XœÊ
-K×ÜÝš[™Ê
-NÂˆ]]]Ý]]HÝš[™ÎŽ›™]Ê
-NÂˆ›Üˆ
-[™^Ú\˜XÝ\ŠH[ˆYÚ]Ë˜Ú\œÊ
-Kœ™]Š
-K™[[Y\˜]J
-HÂˆYˆ[™^ˆ	‰ˆ[™^	HÈOHÂˆÝ]]œ\Ú
-	Ë	ÊNÂˆBˆÝ]]œ\Ú
-Ú\˜XÝ\ŠNÂˆBˆYˆ[[Ý[ÂˆÝ]]œ\Ú
-	ËIÊNÂˆBˆÝ]]˜Ú\œÊ
-Kœ™]Š
-K˜ÛÛXÝ
-
-BŸB‚™›ˆ\œÙWÝ\™Ù]Ý\Ù\›˜[YJ˜[YNˆ	œÝŠHOˆÜ[ÛÝš[™ÏˆÂˆ]\Ù\›˜[YHH˜[YKš[J
-KœÝš\Ü™Yš^
-	Ð	ÊOÎÂˆYˆJK‹LÌŠK˜ÛÛZ[œÊ	\Ù\›˜[YK›[Š
-JBˆ]\Ù\›˜[YBˆ˜ž]\Ê
-Bˆ˜[
-ž]_ž]Kš\×Ø\ØÚZWØ[[[Y\šXÊ
-Hž]HOH‰×ÉÊBˆÂˆ™]\›ˆ›Û™NÂˆBˆÛÛYJ›Ü›X]JÝ\Ù\›˜[Y_HŠJBŸB‚ˆÖØÙ™Ê\Ý
-WB›[Ù\ÝÈÂˆ\ÙHÝ\\ŽŽžÙ›Ü›X]ÜšX[\œÙWÝ\™Ù]Ý\Ù\›˜[Y_NÂ‚ˆÖÝ\ÝBˆ›ˆ˜[Y]\×Ý[YÜ˜[WÝ\Ù\›˜[Y\Ê
-HÂˆ\ÜÙ\Ù\HJˆ\œÙWÝ\™Ù]Ý\Ù\›˜[YJÙÚYZØ›ÝŠKˆÛÛYJÙÚYZØ›Ý‹š[Ê
-JBˆ
-NÂˆ\ÜÙ\J\œÙWÝ\™Ù]Ý\Ù\›˜[YJ›ÙÚYZØ›ÝŠKš\×Û›Û™J
-JNÂˆ\ÜÙ\J\œÙWÝ\™Ù]Ý\Ù\›˜[YJ˜Y[˜[YHŠKš\×Û›Û™J
-JNÂˆB‚ˆÖÝ\ÝBˆ›ˆ›Ü›X]×ÜšX[
-
-HÂˆ\ÜÙ\Ù\HJ›Ü›X]ÜšX[
-LL
-KŒKLŠNÂˆBŸB
+        match state.db.claim_next_fulfillment_job(&worker_id).await {
+            Ok(Some(job)) => {
+                info!(order = %job.public_order_id, provider = state.fulfillment.name(), "fulfillment job claimed");
+                let result = state
+                    .fulfillment
+                    .fulfill(crate::fulfillment::FulfillmentRequest {
+                        order_id: job.order_id,
+                        public_order_id: job.public_order_id,
+                        target_username: job.target_username,
+                        offering_snapshot: job.offering_snapshot,
+                    })
+                    .await;
+                match result {
+                    Ok(result) => {
+                        if let Err(error) =
+                            state.db.finish_fulfillment_job(job.job_id, result).await
+                        {
+                            error!(%error, "could not finish fulfillment job");
+                        }
+                    }
+                    Err(error) => {
+                        error!(%error, "fulfillment provider failed unexpectedly");
+                        let result = FulfillmentResult::RetryableFailure {
+                            reason: error.to_string(),
+                        };
+                        if let Err(error) =
+                            state.db.finish_fulfillment_job(job.job_id, result).await
+                        {
+                            error!(%error, "could not persist fulfillment provider failure");
+                        }
+                    }
+                }
+            }
+            Ok(None) => tokio::time::sleep(Duration::from_secs(3)).await,
+            Err(error) => {
+                error!(%error, "could not poll fulfillment queue");
+                tokio::time::sleep(Duration::from_secs(5)).await;
+            }
+        }
+    }
+}
